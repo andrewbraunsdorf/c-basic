3c9c6453362b7e7c341288ca61d9e3b3bde51bfe
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-05 19:47:16 -0400

    using inheritance

---
+++ man.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Bulldozer
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var man = new Man();
+            var kart = new Kart();
+            var alien = new Alien();
+        }
+    }
+
+    public class Man
+    {
+        public Man()
+        {
+        public bool moves { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+    }
+        
+    }
+
+    public class Kart
+    {
+        public Kart()
+        {
+
+        public bool moves { get; set; }
+        public string manufacturer { get; set; }
+        public string color { get; set; }
+        }
+    }
+
+    public class Alien
+    {
+        public Alien()
+        {
+            public bool moves { get; set; }
+            public string planet { get; set; }
+            public string color { get; set; }
+        }
+    }
+    
+}
