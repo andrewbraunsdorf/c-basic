336dea512c73b19a80a3eec5572e0fb198795c0d
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-07 18:59:39 -0400

    data lost with casting

--- variables.cs
+++ variables.cs
@@ -2,13 +2,12 @@ using System;
 
 namespace Variables
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
-            Console.ReadLine();
-        }
+      const float Pi = 3.14f;
+      Pi = 1;
     }
+  }
 }
