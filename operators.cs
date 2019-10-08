35db01bfb4501badef2ad65094a0156e1bf9144b
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-08 19:59:39 -0400

    added logic and arthimetic operators

---
+++ operators.cs
@@ -0,0 +1,25 @@
+using System;
+
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine((float)a / (float)b);
+            Console.WriteLine(a + b * c);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(!(a != b));
+            Console.WriteLine(c > b && c > a);
+            Console.WriteLine(c > b && c == a);
+            Console.WriteLine(c > b || c == a);
+            Console.ReadLine();
+        }
+    }
+}
