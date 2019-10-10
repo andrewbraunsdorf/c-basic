5f6efae6ae1ae2da4c5c9e9374740779fb5c7a56
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-09 22:29:14 -0400

    verbatim strings

--- DemoStrings.cs
+++ DemoStrings.cs
@@ -11,6 +11,20 @@ namespace DemoStrings
             var fullName = firstName + " " + lastName;
 
             var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = "Hi John\nLook into the following path \nc:\\folder1\\folder2";
+            var texts = @"Hi John
+Look into the following path 
+c:\folder1\folder2";
+
+            Console.WriteLine(text);
+            Console.WriteLine(texts);
+
+            Console.ReadLine();
         }
     }
 }
