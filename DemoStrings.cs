c5c89a426ec68b79233257ce41d512d72004edac
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-09 22:11:59 -0400

    different string types

---
+++ DemoStrings.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DemoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var firstName = "Hugo";
+
+            // same thing
+            String lastNAme = "James";
+            string myName = "Hugo";
+        }
+    }
+}
