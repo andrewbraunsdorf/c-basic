2736b8b175319af7b2504e38cc4abecde5e2aff5
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-12 17:24:05 -0400

    renamed files

--- referenceAndValueTypes.cs
+++ referenceAndValueTypes.cs
@@ -6,35 +6,35 @@ using System.Threading.Tasks;
 
 namespace CSharpFundamentals
 {
-    public class Person
+  public class Person
+  {
+    public int Age;
+  }
+  class Program
+  {
+    static void Main(string[] args)
     {
-        public int Age;
+      var number = 1;
+      Increment(number);
+      // number is 1 as scope is only local to main so it is not put in another place
+      Console.WriteLine(number);
+
+      var person = new Person() { Age = 20 };
+      MakeOld(person);
+      Console.WriteLine(person.Age);
+      // person returns 30 as we call the method here and below
+      Console.ReadLine();
+
     }
-    class Program
+
+    public static void Increment(int number)
     {
-        static void Main(string[] args)
-        {
-            var number = 1;
-            Increment(number);
-            // number is 1 as scope is only local to main so it is not put in another place
-            Console.WriteLine(number);
-
-            var person = new Person() {Age = 20};
-            MakeOld(person);
-            Console.WriteLine(person.Age);
-            // person returns 30 as we call the method here and below
-            Console.ReadLine();
-
-        }
-
-        public static void Increment(int number)
-        {
-            number += 10;
-        }
-
-        public static void MakeOld(Person person)
-        {
-            person.Age += 10;
-        }
+      number += 10;
     }
+
+    public static void MakeOld(Person person)
+    {
+      person.Age += 10;
+    }
+  }
 }
