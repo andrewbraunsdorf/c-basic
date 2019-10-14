69e257503c454395a03567b9f798c328da7f920d
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-13 21:25:28 -0400

    fixed errors

--- Image.cs
+++ Image.cs
@@ -1,10 +1,23 @@
-////Image Portrait or Landscape
-            Console.WriteLine("Enter a height");
-            var height = Convert.ToInt32(Console.ReadLine());
+using System;
 
-Console.WriteLine("Enter a second number");
-            var width = Convert.ToInt32(Console.ReadLine());
 
-var orientation = (height > width) ? "Image Orientation is Portrait." : "Image Orientation is Landscape.";
-Console.WriteLine(orientation);
-            Console.ReadLine();
+namespace EnterNumber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Image Portrait or Landscape
+      Console.WriteLine("Enter a height");
+      var height = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Enter a second number");
+      var width = Convert.ToInt32(Console.ReadLine());
+
+      var orientation = (height > width) ? "Image Orientation is Portrait." : "Image Orientation is Landscape.";
+      Console.WriteLine(orientation);
+
+      Console.ReadLine();
+    }
+  }
+}
