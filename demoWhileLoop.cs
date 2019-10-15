1e5e365a1feaef283a5f663bf47d1eb285fe2279
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-15 19:56:21 -0400

    while loops

---
+++ demoWhileLoop.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Iterations
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      //var i = 0;
+      //while (i <= 10)
+      //{
+      //    if (i % 2 == 0)
+      //        Console.WriteLine(i);
+      //    i++;
+      //}
+
+      // goes forever
+      while (true)
+      {
+        Console.Write("Type your name: ");
+        var input = Console.ReadLine();
+
+        if (!String.IsNullOrWhiteSpace(input))
+        {
+          Console.WriteLine("@Echo: " + input);
+          continue;
+        }
+        break;
+
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
