039ddaf3732ab9abb60e6f07ceab20b6936cd99f
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-16 20:23:14 -0400

    add character from a - z

--- randomClass.cs
+++ randomClass.cs
@@ -15,8 +15,13 @@ namespace CSharpFundamentals
       {
         Console.WriteLine(random.Next());
         Console.WriteLine((char)random.Next(1, 10));
+        // casting to character which allows users to pick a character
+        Console.Write((char)random.Next(97, 122));
+
+        Console.Write((char)('a' + random.Next(0, 26)));
       }
 
+      Console.WriteLine();
       Console.WriteLine((int)'a');
 
       Console.ReadLine();
