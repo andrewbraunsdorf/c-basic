64eb2af27aee7eb624cf749bf71f6d331a3fc16f
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-23 19:58:23 -0400

    check if numbers are consecutive

---
+++ CSIntermediate/IsConsecutive.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace Exercises
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter numbers seperated by hyphen");
+      var input = Console.ReadLine();
+
+      var numbers = new List<int>();
+      foreach (var number in input.Split('-'))
+      {
+        numbers.Add(Convert.ToInt32(number));
+      }
+
+      numbers.Sort();
+
+      for (var i = 1; i < numbers.Count; i++)
+      {
+        if (numbers[i] != numbers[i - 1] + 1)
+        {
+          Console.WriteLine("Not Consecutive");
+          Console.ReadLine();
+          break;
+        }
+      }
+
+
+      Console.WriteLine("Consecutive");
+      Console.ReadLine();
+
+    }
+  }
+}
