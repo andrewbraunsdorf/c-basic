885c340f71bfe07ee6cef740824e557d4b1eb01b
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-23 17:09:00 -0400

    summarize text in function

--- CSIntermediate/summarizeText.cs
+++ CSIntermediate/summarizeText.cs
@@ -8,29 +8,37 @@ namespace CSharpFundamentals
     static void Main(string[] args)
     {
       var sentence = "This is going to be a really, really, really, really, really long text.";
-      const int maxLength = 20;
+      var summary = SummarizeText(sentence);
+      //var summary = SummarizeText(sentence, 25);
 
-      if (sentence.Length < maxLength)
-        Console.WriteLine(sentence);
-      else
+      Console.WriteLine(summary);
+      Console.ReadLine();
+
+    }
+
+    static string SummarizeText(string text, int maxLength = 20)
+    {
+
+
+      if (text.Length < maxLength)
+        return text;
+
+      var words = text.Split(' ');
+      var totalCharacters = 0;
+      var summaryWords = new List<string>();
+
+      foreach (var word in words)
       {
-        var words = sentence.Split(' ');
-        var totalCharacters = 0;
-        var summaryWords = new List<string>();
+        summaryWords.Add(word);
 
-        foreach (var word in words)
-        {
-          summaryWords.Add(word);
-
-          totalCharacters += word.Length + 1;
-          if (totalCharacters > maxLength)
-            break;
-        }
-
-        var summary = String.Join(" ", summaryWords) + "...";
-        Console.WriteLine(summary);
-        Console.ReadLine();
+        totalCharacters += word.Length + 1;
+        if (totalCharacters > maxLength)
+          break;
       }
+      return String.Join(" ", summaryWords) + "...";
+
+      //var summary = String.Join(" ", summaryWords) + "...";
+      //return summary;
     }
   }
 }
