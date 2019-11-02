d6b1fe477a8a1223df90a73caf158bfaff9cb196
AB <AndrewBraunsdorf@gmail.com>
2019-11-01 22:19:24 -0400

    ran instances of customer via this

--- C#Intermediate/constructors/Customer.cs
+++ C#Intermediate/constructors/Customer.cs
@@ -1,24 +1,32 @@
+using System.Collections.Generic;
+
 namespace CSharp
 {
     public class Customer
     {
         public int Id;
         public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
 
         public Customer(int id)
+        // means we will call Customer() without any parameters
+            : this()
         {
             this.Id = id;
         }
 
         public Customer(int id, string name)
+        // first it is going to call the Customer(int id) first
+            : this(id)
         {
-            this.Id = id;
             this.Name = name;
         }
 
-        public Customer()
-        {
-            
-        }
+        
     }
 }
--- C#Intermediate/constructors/program.cs
+++ C#Intermediate/constructors/program.cs
@@ -9,7 +9,13 @@ namespace CSharp
         {
             //Customer customer = new Customer();
             // better way is to use var
-            var customer = new Customer(1, "John");
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
             Console.ReadLine();
