eed8d9614fd3399da1c6e7ce903736e9de9ae9ae
AB <AndrewBraunsdorf@gmail.com>
2019-11-02 22:20:52 -0400

    added global error handling

--- C#Intermediate/methods/program.cs
+++ C#Intermediate/methods/program.cs
@@ -7,15 +7,30 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            var point = new Point(10, 20);
-            //point.Move(new Point(40, 60));
-            point.Move(null);
-            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            
+        }
 
-            point.Move(100, 200);
-            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                //point.Move(new Point(40, 60));
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
 
-            Console.ReadLine();
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured");
+                Console.ReadLine();
+
+            }
+
         }
     }
 }
