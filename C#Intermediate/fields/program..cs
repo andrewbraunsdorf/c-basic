ea1744c2893e42407f7253993d12739ea83b16d8
AB <AndrewBraunsdorf@gmail.com>
2019-11-03 22:08:53 -0500

    added customer order

--- C#Intermediate/fields/program..cs
+++ C#Intermediate/fields/program..cs
@@ -1,9 +1,17 @@
+using System;
+
 namespace Fields
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);
+            Console.ReadLine();
         }
     }
 }
