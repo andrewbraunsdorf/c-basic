849b7f6531bcb3235089d79dca17e70e03115715
AB <AndrewBraunsdorf@gmail.com>
2019-11-08 18:22:24 -0500

    get and set properties

---
+++ C#Intermediate/properties/Properties.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        private DateTime _birthddate;
+
+        public DateTime Birthdate
+        {
+            get { return _birthddate; }
+            set { _birthddate = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
