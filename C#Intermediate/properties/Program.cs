cfd477b0e91e2d3e3e68909487f6d801392e31c0
AB <AndrewBraunsdorf@gmail.com>
2019-11-09 21:29:25 -0500

    find years via properties

---
+++ C#Intermediate/properties/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var person = new Person();
+          person.Birthdate = new DateTime(1982, 1, 1);
+          System.Console.WriteLine(person.Age);
+        }
+    }
+}
