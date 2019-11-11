06f5a5553195e6370861dd664a88d751c899d0b5
AB <AndrewBraunsdorf@gmail.com>
2019-11-10 23:04:52 -0500

    added httpCookie Class

---
+++ C#Intermediate/indexer/HttpCookie.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        //indexer declare
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
