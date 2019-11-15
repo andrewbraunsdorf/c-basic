0a2103eb91c1732b566e368060508e7adc47a204
AB <AndrewBraunsdorf@gmail.com>
2019-11-14 19:44:48 -0500

    created customer class in Amazon in class library

--- C#Intermediate/accessmodifiers/Pillar two/Program.cs
+++ C#Intermediate/accessmodifiers/Pillar two/Program.cs
@@ -1,45 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Amazon;
 
 namespace AccessModifiers
 {
-    public class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        public void Promote()
-        {
-            var rating = CalculateRating(excludeOrders: true);
-            if (rating == 0)
-            {
-                Console.WriteLine("Promoted to Level 1");
-                Console.ReadLine();
-            }
-                
-
-            else
-            {
-                Console.WriteLine("Promoted to Level 2");
-                Console.ReadLine();
-            }
-        }
-
-          // had to change caculateRating class from private to protected to get main to work
-        protected int CalculateRating(bool excludeOrders)
-        {
-            return 0;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             var customer = new Customer();
+            Amazon.RateCalculator calculator = new RateCalculator();
         }
     }
 }
