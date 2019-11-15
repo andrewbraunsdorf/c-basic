9bcbe2e25b3050c3d147648b84b0c057770b1299
AB <AndrewBraunsdorf@gmail.com>
2019-11-14 20:13:57 -0500

    using base, via basing it vehicle class

--- C#Intermediate/constructorsAndInheritance/Vehicle.cs
+++ C#Intermediate/constructorsAndInheritance/Vehicle.cs
@@ -4,9 +4,18 @@ namespace Constructors
 {
     public class Vehicle
     {
-        public Vehicle()
+        private readonly string _registrationNumber;
+
+        //public Vehicle()
+        //{
+        //    Console.WriteLine("Vehicle is being initialized");
+        //}
+
+        public Vehicle(string registrationNumber)
         {
-            Console.WriteLine("Vehicle is being initialized");
+            _registrationNumber = registrationNumber;
+
+            Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
         }
     }
 }
