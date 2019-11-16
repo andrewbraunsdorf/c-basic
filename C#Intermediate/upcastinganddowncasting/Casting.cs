75f6f02dbac0a8320bef48a0734dbb753332b82d
AB <AndrewBraunsdorf@gmail.com>
2019-11-15 22:59:57 -0500

    added Shape class

---
+++ C#Intermediate/upcastinganddowncasting/Casting.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+
+    public class Shape
+    {
+      public int Width { get; set; }
+      public int Height { get; set; }
+      public int X { get; set; }
+      public int Y { get; set; }
+
+      public void Draw()
+      {
+        
+      }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
+
