dbe95db57acbc62990f13ca509ebf8eb54575bfb
AB <AndrewBraunsdorf@gmail.com>
2019-11-19 20:37:39 -0500

    get migrating data

--- C#Intermediate/interfacesandextensibility/Program.cs
+++ C#Intermediate/interfacesandextensibility/Program.cs
@@ -7,7 +7,8 @@ namespace Extensibility
   {
     static void Main(string[] args)
     {
-        
+        var dbMigrator = new DbMigrator(new ConsoleLogger());
+        dbMigrator.Migrate();
     }
   }
 }
