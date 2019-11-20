ee35ab0bedc51290e9af0b74c753fa52846ec165
AB <AndrewBraunsdorf@gmail.com>
2019-11-20 17:58:22 -0500

    added inheritances

--- C#Intermediate/interfacesnotformultipleinheritances/UiControl.cs
+++ C#Intermediate/interfacesnotformultipleinheritances/UiControl.cs
@@ -2,6 +2,18 @@ using System;
 
 namespace MultipleInheritance
 {
+  public class TextBox : UiControl, IDraggable, IDroppable
+  {
+    public void Drag()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Drop()
+    {
+      throw new NotImplementedException();
+    }
+  }
   public class UiControl
   {
     public string Id { get; set; }
