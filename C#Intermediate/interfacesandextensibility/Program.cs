6bf0ec5ccbdd6d169265e1a2544c40df003466b8
AB <AndrewBraunsdorf@gmail.com>
2019-11-19 20:42:40 -0500

    created FileLogger class

--- C#Intermediate/interfacesandextensibility/Program.cs
+++ C#Intermediate/interfacesandextensibility/Program.cs
@@ -3,6 +3,33 @@ using System;
 
 namespace Extensibility
 {
+  public class FileLogger : ILogger
+  {
+    private readonly string _path;
+
+    public FileLogger(string path)
+    {
+      _path = path;
+    }
+
+    public void LogError(string message)
+    {
+      LogError(message, "ERROR");
+    }
+
+    public void LogInfo(string message)
+    {
+      LogInfo(message, "INFO");
+    }
+
+    private void Log(string message, string messageType)
+    {
+      using (var streamWriter = new StreamWriter(_path, true))
+      {
+        streamWriter.WriteLine(messageType + ": " + message);
+      }
+    }
+  }
   class Program
   {
     static void Main(string[] args)
