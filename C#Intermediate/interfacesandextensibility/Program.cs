4c1a066d8e1572ed7fd75a825fe6c13743e3bb05
AB <AndrewBraunsdorf@gmail.com>
2019-11-19 20:44:03 -0500

    added migrating stats in a file

--- C#Intermediate/interfacesandextensibility/Program.cs
+++ C#Intermediate/interfacesandextensibility/Program.cs
@@ -34,7 +34,7 @@ namespace Extensibility
   {
     static void Main(string[] args)
     {
-        var dbMigrator = new DbMigrator(new ConsoleLogger());
+        var dbMigrator = new DbMigrator(new FileLogger("C:\\Projects\\log.txt"));
         dbMigrator.Migrate();
     }
   }
