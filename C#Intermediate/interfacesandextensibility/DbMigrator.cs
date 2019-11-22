be4edd69a73f46faed7d9a0298650fffb1868b9d
AB <AndrewBraunsdorf@gmail.com>
2019-11-21 19:07:47 -0500

    fixed DbMigrator

--- C#Intermediate/interfacesandextensibility/DbMigrator.cs
+++ C#Intermediate/interfacesandextensibility/DbMigrator.cs
@@ -1,20 +1,22 @@
-namespace Extensibility
+using System;
+
+namespace InterfacesAndExtensibility
 {
-  public class DbMigrator
-  {
-    private readonly ILogger _logger;
-
-    public DbMigrator(ILogger logger)
+    public class DbMigrator
     {
-      _logger = logger;
-    }
-    public void Migrate()
-    {
-      _logger.LogInfo("Migration starting at {0}", DateTime.Now);
+        private readonly ILogger _logger;
 
-      // Details of migrating the database
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration starting at {0}" + DateTime.Now);
 
-      _logger.LogInfo("Migration finished at {0}", DateTime.Now);
+            // Details of migrating the database
+
+            _logger.LogInfo("Migration finished at {0}" + DateTime.Now);
+        }
     }
-  }
 }
