22867096a7248ea54a7a47081f6e3b97707135b8
AB <AndrewBraunsdorf@gmail.com>
2019-11-24 20:56:24 -0500

    fixed main class and program app

--- C#Intermediate/exercises/stopwatch/Program.cs
+++ C#Intermediate/exercises/stopwatch/Program.cs
@@ -1,49 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+
 
 namespace Stopwatch
 {
-    public class StopWatch
-    {
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-
-        private bool running = false;
-
-        public void Start(DateTime start)
-        {
-            if (!running)
-            {
-                StartTime = start;
-                running = true;
-            }
-            else
-            {
-                {
-                    throw new InvalidOperationException("Stopwatch is already running!");
-                }
-            }
-        }
-        public void Stop(DateTime stop)
-        {
-            if (running)
-            {
-                EndTime = stop;
-                running = false;
-            }
-        }
-
-        public TimeSpan GetInterval()
-        {
-            var duration = EndTime - StartTime;
-
-            return duration;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
@@ -52,18 +12,18 @@ namespace Stopwatch
 
             for (var i = 0; i < 2; i++)
             {
-                stopwatch.Start(DateTime.Now);
+                stopwatch.Start();
 
-                for (var j = 0; j <= 1000; j++)
-                {
-                    Thread.Sleep(1);
-                }
+                Thread.Sleep(1000);
 
-                stopwatch.Stop(DateTime.Now);
+                stopwatch.Stop();
 
+                Console.WriteLine("Duration:" + stopwatch.GetInterval());
+                Console.WriteLine("Press Enter to run stopwatch one more time.");
+                Console.ReadLine();
             }
 
-            Console.ReadLine();
+
         }
     }
 }
