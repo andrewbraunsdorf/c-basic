98a7217448e30016a1ff62d0288b8f94ae0dae62
AB <AndrewBraunsdorf@gmail.com>
2019-11-24 21:33:00 -0500

    added Push and pop to Stack

---
+++ C#Intermediate/exercises/stack/Program.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private  List<object> list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("You can not add a null object to stack.");
+            }
+
+            list.Add(obj);
+            int elements = list.Count;
+
+            if (elements > 1)
+            {
+                for (int i = elements - 1; i > 0; i--)
+                {
+                    list[i] = list[i - 1];
+                }
+
+                list[0] = obj;
+            }
+        }
+
+        public object Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("There are no objects in the stack yet.");
+            }
+
+            var toReturn = list[0];
+
+            var elements = list.Count;
+
+            for (int i = 0; i < elements -1; i++)
+            {
+                list[i] = (int)list[i + 1];
+            }
+
+            list.Remove(elements - 1);
+
+            return toReturn;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
