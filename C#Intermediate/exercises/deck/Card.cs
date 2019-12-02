17de32e274ef6dfaa68845bade348ede528c8def
AB <AndrewBraunsdorf@gmail.com>
2019-12-02 18:39:06 -0500

    added card and suit into its own file

---
+++ C#Intermediate/exercises/deck/Card.cs
@@ -0,0 +1,9 @@
+namespace CardGameApp
+{
+    public class Card
+    {
+        public string DisplayName { get; set; }
+        public Suit Suit { get; set; }
+        public int Value { get; set; }
+    }
+}
