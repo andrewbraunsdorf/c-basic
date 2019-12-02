6427e1e8e1962442e222590219db55e3370155ae
AB <AndrewBraunsdorf@gmail.com>
2019-12-01 20:30:50 -0500

    create player class

--- C#Intermediate/exercises/deck/Program.cs
+++ C#Intermediate/exercises/deck/Program.cs
@@ -91,7 +91,43 @@ namespace CardGameApp
         }
     }
 
+    public class Player
+    {
+        public string Name { get; set; }
+        public Queue<Card> Deck { get; set; }
 
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public Queue<Card> Deal(Queue<Card> cards)
+        {
+            Queue<Card> player1cards = new Queue<Card>();
+            Queue<Card> player2cards = new Queue<Card>();
+
+            int counter = 2;
+
+            while (player1cards.Count <= 5 && player2cards.Count <= 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    player2cards.Enqueue(cards.Dequeue());
+                    Console.WriteLine(cards);
+                }
+                else
+                {
+                    player1cards.Enqueue(cards.Dequeue());
+                    Console.WriteLine(cards);
+                }
+
+                counter++;
+            }
+
+            Deck = player2cards;
+            return player1cards;
+        }
+    }
 
     class Program
     {
