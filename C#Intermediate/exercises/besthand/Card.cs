cc52a5242a831a6d46c4fd1216abcf70c22e5f85
AB <AndrewBraunsdorf@gmail.com>
2019-12-15 21:32:31 -0500

    moved card class

---
+++ C#Intermediate/exercises/besthand/Card.cs
@@ -0,0 +1,10 @@
+namespace Besthand
+{
+    public class Card
+    {
+        
+        public int Suit { get; set; }
+        public int Face { get; set; }
+        public int Value { get; set; }
+    }
+}
