85a856c5c80c0e8df9a50689c148cd0b3514ee20
AB <AndrewBraunsdorf@gmail.com>
2019-12-16 21:42:42 -0500

    moved player

--- C#Intermediate/exercises/besthand/Program.cs
+++ C#Intermediate/exercises/besthand/Program.cs
@@ -8,27 +8,6 @@ using System.Threading.Tasks;
 
 namespace Besthand
 {
-
-    
-
-    class Player
-    {
-        public int Score { get; set; }
-
-        public Player()
-        {
-            this.Score = 0;
-        }
-
-        public void ScoreCalculator(List<Card> playerHand)
-        {
-            foreach (var item in playerHand)
-            {
-                this.Score += item.Value;
-            }
-        }
-    }
-
     class BestHandGame
     {
         Random rand;
