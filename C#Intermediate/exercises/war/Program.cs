5900d63122f5066e9652b423cd399991a48b4780
AB <AndrewBraunsdorf@gmail.com>
2019-12-19 22:24:45 -0500

    add deal to player

--- C#Intermediate/exercises/war/Program.cs
+++ C#Intermediate/exercises/war/Program.cs
@@ -26,12 +26,34 @@ namespace War
         public string Name { get; set; }
         public Queue<Card> Deck { get; set; }
 
-                public Player() { }
+        public Player() { }
 
         public Player(string name)
         {
             Name = name;
         }
+
+        public Queue<Card> Deal(Queue<Card> cards)
+        {
+            Queue<Card> player1cards = new Queue<Card>();
+            Queue<Card> player2cards = new Queue<Card>();
+
+            int counter = 2;
+            while (cards.Any())
+            {
+                if (counter % 2 == 0) //Card etiquette says the player who is NOT the dealer gets first card
+                {
+                    player2cards.Enqueue(cards.Dequeue());
+                }
+                else
+                {
+                    player1cards.Enqueue(cards.Dequeue());
+                }
+
+                counter++;
+            }
+        }
+
     }
 
     // implement a deck of cards using the built-in Queue<> object
