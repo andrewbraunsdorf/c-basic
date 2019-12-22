2bc6da703fcc564cb12f796c4e0e52a6ae26d6ba
AB <AndrewBraunsdorf@gmail.com>
2019-12-21 22:50:13 -0500

    merged files

--- C#Intermediate/exercises/war/Program.cs
+++ C#Intermediate/exercises/war/Program.cs
@@ -6,19 +6,6 @@ using System.Threading.Tasks;
 
 namespace War
 {
-
-    // implement a deck of cards using the built-in Queue<> object
-    public static class Extensions
-    {
-        public static void Enqueue(this Queue<Card> cards, Queue<Card> newCards)
-        {
-            foreach (var card in newCards)
-            {
-                cards.Enqueue(card);
-            }
-        }
-    }
-
     public class DeckCreator
     {
         public static Queue<Card> CreateCards()
