0a3afb1920e994638d19a5b6bc9a20959e65e24c
AB <AndrewBraunsdorf@gmail.com>
2019-12-25 19:06:54 -0500

    fixed naming convention

--- C#Intermediate/exercises/Duplicate/Program.cs
+++ C#Intermediate/exercises/Duplicate/Program.cs
@@ -6,15 +6,15 @@ class MainClass
     static int FirstDuplicateNumber(int[] array)
     {
         List<int> numbersList = new List<int>();
-        List<int> NumberCheck = new List<int>();
+        List<int> numberCheck = new List<int>();
 
         foreach (int number in array) numbersList.Add(number);
 
 
         foreach (int item in numbersList)
         {
-            if (NumberCheck.IndexOf(item) != -1) return item;
-            NumberCheck.Add(item);
+            if (numberCheck.IndexOf(item) != -1) return item;
+            numberCheck.Add(item);
         }
         return -1;
     }
